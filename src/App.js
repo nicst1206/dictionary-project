01b08a00e7483a57b88078ef8986a1eece505575
nicst1206 <nicole@rust-frankfurt.de>
2025-01-10 18:12:37 +0100

    removed all unnecessary code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>This will be my dictionary Website</h1>
+    </div>
+  );
+}
+
+export default App;
