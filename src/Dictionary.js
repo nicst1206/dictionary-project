831dd060a71d2412e63a8d9e92773619cee4c54f
nicst1206 <nicole@rust-frankfurt.de>
2025-01-11 16:27:47 +0100

    added Api query

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} in Dictionary`);
+
+    let apiKey = "obbt00a19b0447e3fbfabf89040f3c88";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function changeKeyword(event) {
