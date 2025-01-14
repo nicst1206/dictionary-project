8aca47e8992519edaafc085ac0b58be4af3f36dd
nicst1206 <nicole@rust-frankfurt.de>
2025-01-14 16:33:18 +0100

    Added picture API and Display of pictures

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,15 +2,21 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
   let defaultWordValue = `i.e. ${props.defaultKeyword}`;
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
@@ -18,6 +24,10 @@ export default function Dictionary(props) {
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
     setLoaded(true);
+
+    const imagesApiKey = "obbt00a19b0447e3fbfabf89040f3c88";
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(handlePexelsResponse);
   }
 
   function changeKeyword(event) {
@@ -44,6 +54,7 @@ export default function Dictionary(props) {
           <input className="searchButton" type="submit" value="Search"></input>
         </form>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
